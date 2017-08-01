790eead910f0f43f727e2a755413de5cc84dc108
Pierce Darragh <pierce.darragh@gmail.com>
2017-07-31 23:28:43 -0700

    Implement board generation

--- src/Game.hs
+++ src/Game.hs
@@ -1,6 +1,7 @@
 module Game where
 
 import Control.Monad.Random
+import Data.List (sort)
 
 data BoardCell = Empty
                | Flag Int
@@ -13,34 +14,63 @@ data ChoiceCell a = Chosen a
 
 type Cell = ChoiceCell BoardCell
 
-type BoardRow = [Cell]
-type Board    = [BoardRow]
+type Row   = [Cell]
+type Board = [Row]
 
 data Game = Game { board    :: Board
                  , finished :: Bool
                  , won      :: Bool
-                 }
-                 deriving (Show, Eq)
+                 } deriving (Show, Eq)
 
---createGame :: Int -> Int -> Int -> Game
---createGame x y mines = Game (createBoard x y mines) False False
+createGame :: MonadRandom m => Int -> Int -> Int -> m Game
+createGame x y m = do
+    ms <- minePositions x y m
+    return (Game (createBoard x y ms) False False)
 
---createBoard :: Int -> Int -> Int -> Board
---createBoard x y mines = setFlags (iterate createRow y)
+createBoard :: Int -> Int -> [(Int, Int)] -> Board
+createBoard width height ms = createBoard' width height 0 ms []
 
---createRow :: Int -> 
+createBoard' :: Int -> Int -> Int -> [(Int, Int)] -> Board -> Board
+createBoard' width height y ms rs
+    | y == height = rs
+    | otherwise = (createRow width y ms) : (createBoard' width height (y + 1) ms rs)
 
---minePositions :: Int -> Int -> Int -> [(Int, Int)]
---minePositions x y mines = minePositions' x y [] mines
+createRow :: Int -> Int -> [(Int, Int)] -> Row
+createRow width y ms = createRow' width 0 y ms []
 
---getPos :: Int -> IO Int
+createRow' :: Int -> Int -> Int -> [(Int, Int)] -> Row -> Row
+createRow' width x y ms ps
+    | x == width = ps
+    | otherwise = (createCell x y ms) : (createRow' width (x + 1) y ms ps)
+
+createCell :: Int -> Int -> [(Int, Int)] -> Cell
+createCell x y ms = Unchosen marker where
+    marker = case surroundingMineCount x y ms of
+               Just n -> if n == 0
+                           then Empty
+                           else Flag n
+               Nothing -> Mine
+
+surroundingMineCount :: Int -> Int -> [(Int, Int)] -> Maybe Int
+surroundingMineCount x y ms
+    | elem (x, y) ms = Nothing
+    | otherwise = Just (length (filter (`elem` ms) (surroundingCells x y)))
+
+surroundingCells :: Int -> Int -> [(Int, Int)]
+surroundingCells x y = map (addPos (x, y)) [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+
+addPos :: (Int, Int) -> (Int, Int) -> (Int, Int)
+addPos (a, b) (c, d) = (a + c, b + d)
+
 getPos :: MonadRandom m => Int -> m Int
 getPos n = getRandomR (0, n - 1)
 
 minePositions :: MonadRandom m => Int -> Int -> Int -> m [(Int, Int)]
 minePositions x y m
     | m > (x * y) = error "too many mines for board size"
-    | otherwise = minePositions' x y m
+    | otherwise = do
+        ps <- minePositions' x y m
+        return (sort ps)
 
 minePositions' :: MonadRandom m => Int -> Int -> Int -> m [(Int, Int)]
 minePositions' _ _ 0 = return []
@@ -52,12 +82,3 @@ minePositions' x y m = do
     if elem pos ps
        then minePositions' x y m
        else return (pos : ps)
-
---unique :: (Eq a) => [a] -> [a] -> [a]
---unique seen (x : xs)
---    | x `elem` seen = unique seen xs
---    | otherwise = x : unique (x : seen) xs
-
---minePositions' x y m = do
---    ps <- getRandomRs (x, y)
---    return (take m (unique [] ps))
