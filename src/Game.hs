28073009f8cb8924cb8552da67d0d6cc42693c8c
Pierce Darragh <pierce.darragh@gmail.com>
2020-04-28 17:14:52 -0600

    Add in-progress work that never got added

--- src/Game.hs
+++ src/Game.hs
@@ -1,17 +1,15 @@
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE RecordWildCards, MultiWayIf #-}
 
-module Game
-    ( makeBoard
-    , perspShow
-    , Perspective(..)
-    )
-where
+module Game where
 
 import           Data.List
 import           Data.List.Split
 import           Control.Monad.Random
 import           System.Random.Shuffle
+
+--------------
+-- MEASURES --
+--------------
 
 -- Introduce a measure for liquid type constraints.
 {-@ measure llen @-}
@@ -19,15 +17,25 @@ llen :: [a] -> Int
 llen []       = 0
 llen (x : xs) = 1 + llen xs
 
+-----------
+-- TYPES --
+-----------
+
 -- Coordinates, used in mine generation and player interaction.
 -- {-@ type Coord = { p : (Int, Int) | fst p >= 0 && snd p >= 0 } @-}
 type Coord = (Int, Int)
 
+-- # of mines.
 {-@ type MineCount = { v : Int | 0 <= v && v <= 8 } @-}
 type MineCount = Int
 
+-- A board dimension (boards are (size x size) in area).
 {-@ type Size = { v : Int | 0 < v } @-}
 type Size = Int
+
+----------------
+-- DATA TYPES --
+----------------
 
 -- What the cell contains (either a mine or a count of how many mines are
 -- nearby).
@@ -59,6 +67,10 @@ newtype Board = Board [Row]  -- A collection of rows.
 -- as the computer (full access).
 data Perspective = Player | Computer
 
+---------------------------
+-- CLASSES AND INSTANCES --
+---------------------------
+
 -- A class like Show, except which takes a Perspective as argument.
 -- This is used for printing cells.
 class PerspShow a where
@@ -88,36 +100,50 @@ instance Show Row where
 instance Show Board where
     show = perspShow Player
 
+------------------------------
+-- CONVENIENCE CONSTRUCTORS --
+------------------------------
+
+mkFreshMineCell :: Cell
+mkFreshMineCell = Cell { fill = Mine, state = Undisturbed }
+
+{-@ mkFreshProxCell :: MineCount -> Cell @-}
+mkFreshProxCell :: MineCount -> Cell
+mkFreshProxCell p = Cell { fill = Proximity p, state = Undisturbed }
+
+-----------------
+-- BOARD SETUP --
+-----------------
+
+-- Generate a list of coordinates, used for selecting mine locations.
 -- {-@ generateCoords :: w : Size -> h : Size -> { cs : [Coord] | llen cs == (w * h) } @-}
 generateCoords :: Size -> Size -> [Coord]
 generateCoords w h = [ (x, y) | x <- [0 .. w], y <- [0 .. h] ]
 
+-- Select the coordinates that will be laid with mines.
 {-@ chooseMines :: MonadRandom m => Size -> [Coord] -> m [Coord] @-}
 chooseMines :: MonadRandom m => Size -> [Coord] -> m [Coord]
 chooseMines n coords = sort . take n <$> shuffleM coords
 
-mkMineCell :: Cell
-mkMineCell = Cell { fill = Mine, state = Undisturbed }
+makeBoard :: Int -> Size -> Size -> Board
+makeBoard mc w h =
 
-{-@ mkProxCell :: MineCount -> Cell @-}
-mkProxCell :: MineCount -> Cell
-mkProxCell c = Cell { fill = Proximity c, state = Undisturbed }
 
-{-@ mkMaybeCell :: [Coord] -> Coord -> Maybe Cell @-}
-mkMaybeCell :: [Coord] -> Coord -> Maybe Cell
-mkMaybeCell []       coord = Nothing
-mkMaybeCell (c : cs) coord = if
-    | c == coord -> Just mkMineCell
-    | otherwise  -> mkMaybeCell cs coord
+-- {-@ mkMaybeCell :: [Coord] -> Coord -> Maybe Cell @-}
+-- mkMaybeCell :: [Coord] -> Coord -> Maybe Cell
+-- mkMaybeCell []       coord = Nothing
+-- mkMaybeCell (c : cs) coord = if
+--     | c == coord -> Just mkMineCell
+--     | otherwise  -> mkMaybeCell cs coord
 
-{-@ groupN :: w : Size -> h : Size -> { xs : [a] | llen xs == w * h } -> [[a]] @-}
-groupN :: Int -> Int -> [a] -> [[a]]
-groupN w _ = chunksOf w
+-- {-@ groupN :: w : Size -> h : Size -> { xs : [a] | llen xs == w * h } -> [[a]] @-}
+-- groupN :: Int -> Int -> [a] -> [[a]]
+-- groupN w _ = chunksOf w
 
-{-@ mkMineBoard :: Size -> Size -> [Coord] -> [Maybe Cell] @-}
-mkMineBoard :: Size -> Size -> [Coord] -> [Maybe Cell]
-mkMineBoard w h mines = map (mkMaybeCell mines) (generateCoords w h)
+-- {-@ mkMineBoard :: Size -> Size -> [Coord] -> [Maybe Cell] @-}
+-- mkMineBoard :: Size -> Size -> [Coord] -> [Maybe Cell]
+-- mkMineBoard w h mines = map (mkMaybeCell mines) (generateCoords w h)
 
-{-@ makeBoard :: { w : Int | w > 0 } -> { h : Int | h > 0 } -> [[Cell]] @-}
-makeBoard :: Int -> Int -> [[Cell]]
-makeBoard w h = replicate w $ replicate h mkMineCell
+-- {-@ makeBoard :: { w : Int | w > 0 } -> { h : Int | h > 0 } -> [[Cell]] @-}
+-- makeBoard :: Int -> Int -> [[Cell]]
+-- makeBoard w h = replicate w $ replicate h mkMineCell
